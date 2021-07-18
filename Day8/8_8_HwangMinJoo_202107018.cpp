789de1080337ba26546df9d0ad360f237f905558
Minjoo-Hwang <21900812@handong.edu>
2021-07-18 14:09:59 +0900

    problem solved

---
+++ Day8/8_8_HwangMinJoo_202107018.cpp
@@ -0,0 +1,28 @@
+/*
+난이도 1
+백준 15552. 빠른 A+B
+
+문제 설명을 그대로 활용하면 시간 속도를 줄일 수 있다.
+https://www.acmicpc.net/problem/15552
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, x, y;
+    cin >> a;
+
+    for(int i = 0; i<a; i++){
+        cin >> x >> y;
+        cout<<x+y<<"\n";
+    }
+
+    return 0;
+}
